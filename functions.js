60087ef69de617eb912f7287d8899f749b25175e
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 18:07:07 +0100

    Make the code dry, add foreach to do the events.

--- functions.js
+++ functions.js
@@ -1,16 +1,16 @@
 function openCloseMenu() {
   const x = document.querySelector('.mobile-menu');
   const y = document.querySelector('body');
-  if (x.style.display === 'none') {
-    x.style.display = 'flex';
-    y.style.overflow = 'hidden';
-  } else {
+  if (x.style.display === 'flex') {
     x.style.display = 'none';
     y.style.overflow = 'scroll';
+  } else {
+    x.style.display = 'flex';
+    y.style.overflow = 'hidden';
   }
 }
-document.getElementById('onclick-open').addEventListener('click', openCloseMenu);
-document.getElementById('onclick-close').addEventListener('click', openCloseMenu);
-document.getElementById('onclick-portfolio').addEventListener('click', openCloseMenu);
-document.getElementById('onclick-about').addEventListener('click', openCloseMenu);
-document.getElementById('onclick-contact').addEventListener('click', openCloseMenu);
+function addevent(id) {
+  document.getElementById(id).addEventListener('click', openCloseMenu);
+}
+const ids = ['onclick-open', 'onclick-close', 'onclick-portfolio', 'onclick-about', 'onclick-contact'];
+ids.forEach(addevent);
