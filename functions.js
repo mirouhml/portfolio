97e17b1adc4ba446480fbe2c78594641fabc20be
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 17:12:41 +0100

    Add event listener instead of onclick in html.

--- functions.js
+++ functions.js
@@ -1,12 +1,16 @@
 function openCloseMenu() {
-  let x = document.querySelector('.mobile-menu');
-  let y = document.querySelector('body');
+  const x = document.querySelector('.mobile-menu');
+  const y = document.querySelector('body');
   if (x.style.display === 'none') {
     x.style.display = 'flex';
     y.style.overflow = 'hidden';
-  }
-  else {
+  } else {
     x.style.display = 'none';
     y.style.overflow = 'scroll';
-	}
+  }
 }
+document.getElementById('onclick-open').addEventListener('click', openCloseMenu);
+document.getElementById('onclick-close').addEventListener('click', openCloseMenu);
+document.getElementById('onclick-portfolio').addEventListener('click', openCloseMenu);
+document.getElementById('onclick-about').addEventListener('click', openCloseMenu);
+document.getElementById('onclick-contact').addEventListener('click', openCloseMenu);
