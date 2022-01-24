722663178fdc376615b7bcfc854f1c245f846de6
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 16:54:34 +0100

    Fix JS linters errors.

--- functions.js
+++ functions.js
@@ -1,12 +1,12 @@
 function openCloseMenu() {
-    var x = document.querySelector(".mobile-menu");
-    var y = document.querySelector("body");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
-        y.style.overflow = "hidden"
-    }
-    else {
-        x.style.display = "none";
-        y.style.overflow = "scroll"
-    }
+  let x = document.querySelector('.mobile-menu');
+	let y = document.querySelector('body');
+	if (x.style.display === 'none') {
+			x.style.display = 'flex';
+			y.style.overflow = 'hidden';
+	}
+	else {
+			x.style.display = 'none';
+			y.style.overflow = 'scroll';
+	}
 }
