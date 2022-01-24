e7958698477b292f11a44fa6c7bcd104bc960cc5
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 14:21:25 +0100

    Add content of openCloseMenu function.

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
 function openCloseMenu() {
-
+    var x = document.querySelector(".mobile-menu");
+    if (x.style.display === "none")
+        x.style.display = "flex";
+    else 
+        x.style.display = "none";
 }
