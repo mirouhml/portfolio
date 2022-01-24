11364c81f6f58c354fadbc109e76b76eb4b7e7fe
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 14:17:29 +0100

    Add js function openCloseMenu().

--- functions.js
+++ functions.js
@@ -1,8 +1,3 @@
 function openCloseMenu() {
-    var x = document.querySelector(".mobile-menu");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-  }
+
+}
