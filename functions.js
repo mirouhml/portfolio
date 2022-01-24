c9660d0c3fdcfb9d1b28b8c5b39e425d6e4d5583
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 16:57:32 +0100

    Change tab to spaces.

--- functions.js
+++ functions.js
@@ -1,12 +1,12 @@
 function openCloseMenu() {
   let x = document.querySelector('.mobile-menu');
-	let y = document.querySelector('body');
-	if (x.style.display === 'none') {
-			x.style.display = 'flex';
-			y.style.overflow = 'hidden';
-	}
-	else {
-			x.style.display = 'none';
-			y.style.overflow = 'scroll';
+  let y = document.querySelector('body');
+  if (x.style.display === 'none') {
+    x.style.display = 'flex';
+    y.style.overflow = 'hidden';
+  }
+  else {
+    x.style.display = 'none';
+    y.style.overflow = 'scroll';
 	}
 }
